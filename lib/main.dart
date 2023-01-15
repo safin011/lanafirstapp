45094c86c05d329aa4ce40dd27919fbadeb76af7
lana-github <79203335+lana-github@users.noreply.github.com>
2023-01-16 00:41:09 +0300

    gorimm

--- lib/main.dart
+++ lib/main.dart
@@ -108,7 +108,8 @@ class _MyHomePageState extends State<MyHomePage> {
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        child: const Icon(Icons.minimize),
+
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
